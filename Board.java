70ba4f0474d168b28db7e981b4167e5209320cb6
RyOsUkE <ryosukek74@gmail.com>
2022-04-15 19:02:29 +0900

    Update Board.java

--- Board.java
+++ Board.java
@@ -13,7 +13,7 @@ public class Board {
   static String rev_stone;
 
   static public void initialize() {
-    
+
   // オセロ版をリセットする処理：for文の入れ子構造
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
@@ -47,7 +47,7 @@ static public void showBoard() {
   //オセロ版を描写するプログラム
   int i = 0;
   System.out.println(" |0 |1 |2 |3 |4 |5 |6 |7 |");
-  System.out.println("――――――――――――――");
+  System.out.println("―――――――――――――");
   for (String[] sarr : board) {
 
     System.out.print(i + " |");
